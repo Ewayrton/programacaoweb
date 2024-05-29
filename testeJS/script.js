fcec34fe8047dfda461444ee581d3e0998be7a23
Ewayrton <164266609+Ewayrton@users.noreply.github.com>
2024-05-29 22:47:39 +0000

    teste java script

--- testeJS/script.js
+++ testeJS/script.js
@@ -3,7 +3,5 @@ const btSubst = document.getElementById("btSubst");
 
 btSubst.onclick = () => {
     var numero = Math.floor(Math.random() * 100) + 1;
-  
-    // Exibe o número aleatório na página
-    document.getElementById("numeroAleatorio").innerHTML = "Número Aleatório: " + numero;
+    h2Subst.innerHTML = "Número Aleatório: " + numero;
 };
