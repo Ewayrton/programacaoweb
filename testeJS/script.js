2ce2c57d2fcee3b90fc68721ddd909cd3cac951c
Ewayrton <164266609+Ewayrton@users.noreply.github.com>
2024-05-30 00:01:27 +0000

    teste js att

--- testeJS/script.js
+++ testeJS/script.js
@@ -1,7 +1,14 @@
 const h2Subst = document.getElementById("h2Subst");
 const btSubst = document.getElementById("btSubst");
+const h2Subst2 = document.getElementById("h2Subst2")
+const btSubst2 = document.getElementById("btSubst2")
 
 btSubst.onclick = () => {
     var numero = Math.floor(Math.random() * 100) + 1;
-    h2Subst.innerHTML = "Número Aleatório: " + numero;
+    h2Subst.innerHTML = "Número Sorteado: " + numero;
+};
+
+btSubst2.onclick = () => {
+    var numero = Math.floor(Math.random() * 10) + 1;
+    h2Subst2.innerHTML = "Número Sorteado: " + numero;
 };
